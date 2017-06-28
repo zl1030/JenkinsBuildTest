dd8c4e73fbc8129a3da8803dc0b09f2518ca552f
zhoulei <zhoulei@xuanxiaotec.com>
2017-06-28 16:03:14 +0800

    add Test Project

---
+++ src/main/java/com/zl1030/Test.java
@@ -0,0 +1,10 @@
+package com.zl1030;
+
+/**
+ * @author by lei zhou on 2017/06/28 16:02.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello Jenkins!!!!~~~~~~");
+    }
+}
