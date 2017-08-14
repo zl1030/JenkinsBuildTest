4de3934668bd08de7c8bbae3438da682fec905a7
zhoulei <zhoulei@xuanxiaotec.com>
2017-08-14 16:32:54 +0800

    aaabbbccc

--- src/main/java/com/zl1030/Test.java
+++ src/main/java/com/zl1030/Test.java
@@ -5,6 +5,6 @@ package com.zl1030;
  */
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hello Jenkins!!!!~~~~~~");
+        System.out.println("Hello CI!!!!~~~~~~");
     }
 }
